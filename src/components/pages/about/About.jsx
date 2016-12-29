a53330c8f6ff502b009486e9669fe7171cc346f2
Trevor Glass <glasser1988@gmail.com>
2016-12-28 21:27:40 -0500

    about looking a little better

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -6,23 +6,25 @@ import STYLES from './About.scss';
 
 const About = () => (
   <div className={STYLES.About}>
-    <div className={STYLES.inside}>
-      <div className={STYLES.hockey}></div>
-      <div>
-        <h2 className={STYLES.title}>The Story Of </h2>
-        <h3 className={STYLES.name}>Trevor Glass</h3>
-        <div className={STYLES.story}>
-          <p className={STYLES.start}>From hockey player to</p>
-          <p className={STYLES.intro}>
-            Software Engineer my values have &nbsp;&nbsp;&nbsp; remained the same.
-          </p>
-          <p className={STYLES.always}>Always use</p>
-          <p className={STYLES.pointOne}>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; my creativity and hard work to create opportunities. Dream of the impossible. And always
-          </p>
-          <p className={STYLES.finish}>
-            believe that anything is possible.
-          </p>
+    <div>
+      <div className={STYLES.inside}>
+        <div className={STYLES.hockey}></div>
+        <div>
+          <h2>The Story Of</h2>
+          <h3>Trevor Glass</h3>
+          <div className={STYLES.story}>
+            <p className={STYLES.one}>From hockey player to</p>
+            <p className={STYLES.two}>
+              Software Engineer my values have &nbsp;&nbsp;&nbsp; remained the same.
+            </p>
+            <p className={STYLES.three}>Always use</p>
+            <p className={STYLES.four}>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; my creativity and hard work to create opportunities. Dream of the impossible. And
+            </p>
+            <p className={STYLES.five}>
+              always believe that anything is possible.
+            </p>
+          </div>
         </div>
       </div>
     </div>
