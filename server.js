d465d8d074ce19cfe853f3fafa16837714abb2d6
Trevor Glass <glasser1988@gmail.com>
2017-01-01 17:52:13 -0500

    another webpack config

---
+++ server.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+
+console.log("port", process.env.PORT);
+
+const port = isDeveloping ? 8080 : 8081;
+const app = express();
+
+if (isDeveloping) {
+  let webpack = require('webpack');
+  let webpackMiddleware = require('webpack-dev-middleware');
+  let webpackHotMiddleware = require('webpack-hot-middleware');
+  let config = require('./webpack.config.js');
+
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    noInfo: true,
+    quiet: false,
+    lazy: false,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true
+    },
+    stats: {
+      colors: true,
+    }
+  });
+
+  const bundlePath = path.join(__dirname, './public/build/index.html')
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', function response(req, res) {
+    res.write(middleware.fileSystem.readFileSync(bundlePath));
+    res.end();
+  });
+} else {
+  app.use(express.static(__dirname + './public/build'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, './public/build/index.html'));
+  });
+}
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
---
+++ webpack.config.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const validate = require('webpack-validator');
+
+// require('es6-promise').polyfill();
+
+const javascriptEntryPath = path.resolve(__dirname, 'src', 'index.js');
+const htmlEntryPath = path.resolve(__dirname, 'index.html');
+const buildPath = path.resolve(__dirname, 'public', 'build');
+
+const config = {
+  cache: true,
+  debug: true,
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    javascriptEntryPath,
+    htmlEntryPath
+  ],
+  output: {
+    path: buildPath,
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      test: /\.js[x]?$/,
+      loaders: ['react-hot', 'babel-loader'],
+    }, {
+      test: /\.css$/,
+      loaders: ['style', 'css']
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'postcss', 'sass']
+    }, {
+      test: /\.woff$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+    }, {
+      test: /\.woff2$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+    }, {
+      test: /\.(eot|ttf|svg|gif|png)$/,
+      loader: "file-loader"
+    }, {
+      test: /\.html$/,
+      loader: 'file?name=[name].[ext]',
+    }]
+  },
+
+  postcss: function() {
+    return [autoprefixer({
+      browsers: ['last 3 versions']
+    })];
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
+
+// if (TARGET === 'dev' || !TARGET) {
+//   module.exports = merge(config, {
+//     devtool: '#source-map',
+//     devServer: {
+//       historyApiFallback: true
+//     },
+//     output: {
+//       publicPath: 'http://localhost:8090/assets'
+//     },
+//     plugins: [
+//       new NpmInstallPlugin({
+//         save: true // --save
+//       })
+//     ]
+//   });
+// }
+//
+// if (TARGET === 'build') {
+//   module.exports = merge(config, {
+//     devtool: 'source-map',
+//     output: {
+//       path: `./dist`,
+//     },
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         title: 'Trevor Glass',
+//         template: 'index-template.ejs'
+//       }),
+//       // new webpack.DefinePlugin({
+//       //   'process.env': {
+//       //     NODE_ENV: JSON.stringify('production'),
+//       //   },
+//       // }),
+//       // new webpack.NoErrorsPlugin(),
+//       // new webpack.optimize.OccurenceOrderPlugin(),
+//       // new webpack.optimize.DedupePlugin(),
+//       // new webpack.optimize.UglifyJsPlugin()
+//     ]
+//   });
+// }
+
+module.exports = validate(config);
