5899a981186ebb4fa9c36c815faedb04f260cd9a
Trevor Glass <glasser1988@gmail.com>
2017-01-01 22:58:51 -0500

    with a 404

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -23,7 +23,7 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].[chunkhash].js'
+    filename: 'bundle.js'
   },
   plugins: [
     // Hash the files using MD5 so that their names change when the content changes.
