df3a88fb59f8d4d626f8f37c50bb5c297bcac660
Trevor Glass <glasser1988@gmail.com>
2017-01-01 20:31:59 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -42,8 +42,14 @@ const config = {
       test: /\.scss$/,
       loaders: ['style', 'css', 'postcss', 'sass']
     }, {
-      test: /.woff$|.woff2$|.ttf$|.eot$|.svg$|.png$/,
-      loader: 'url-loader'
+      test: /\.woff$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+    }, {
+      test: /\.woff2$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+    }, {
+      test: /\.(eot|ttf|svg|gif|png)$/,
+      loader: "file-loader"
     }, {
       test: /\.html$/,
       loader: 'file?name=[name].[ext]',
