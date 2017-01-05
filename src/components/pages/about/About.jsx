7173c2958942035801890dd965a109fefcabe0cd
Trevor Glass <glasser1988@gmail.com>
2017-01-04 21:21:47 -0500

    just need mobile

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -40,13 +40,22 @@ class About extends Component {
       <div className={STYLES.About}>
         <div className={STYLES.inside}>
           <div className={STYLES.hockey}></div>
+          <div className={STYLES.stickOne}>
+            <span className={STYLES.brand}>TREVOR GLASS</span>
+          </div>
+          <div className={STYLES.testTwo}></div>
+          <div className={STYLES.blade}>
+            <div className={STYLES.tape}></div>
+            <div className={STYLES.tapeTwo}></div>
+            <div className={STYLES.tapeThree}></div>
+          </div>
           <div>
             <h2>The Story Of</h2>
             <h3>Trevor Glass</h3>
             <hr className={STYLES.line} />
             <div className={STYLES.story}>
               <p className={STYLES.one}>From hockey player to</p>
-              <p className={STYLES.two}>Software Engineer my values have &nbsp;&nbsp;&nbsp; remained the same.</p>
+              <p className={STYLES.two}>Software Engineer my values have &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; remained the same.</p>
               <p className={STYLES.three}>Always use</p>
               <p className={STYLES.four}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; my creativity and hard work to create opportunities. Dream of the impossible. And</p>
               <p className={STYLES.five}>always believe that anything is possible.</p>
@@ -56,15 +65,20 @@ class About extends Component {
               className={STYLES.videoButton}>
               <Glyphicon glyph='film' />
             </Button>
+            <Glyphicon className={STYLES.arrowOne} glyph='share-alt' />
+            <h4 className={STYLES.highlights}>Hockey Highlights</h4>
             <VideoModal
               show={modalOpen}
               onHide={this.closeModal}
             />
-
+          <a href="../../../assets/docs/resume2017.pdf" target='_blank'>
             <Button
               className={STYLES.pdfButton}>
               <i className="fa fa-file-pdf-o"></i>
             </Button>
+          </a>
+            <Glyphicon className={STYLES.arrowTwo} glyph='share-alt' />
+            <h4 className={STYLES.resume}>Printable Resume</h4>
           </div>
         </div>
       </div>
